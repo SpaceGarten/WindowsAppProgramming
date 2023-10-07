3a40a5d0a03d1c5b3fbe0e8dc35ec8f1c8588aef
HughEver3tt <59859156+HughEver3tt@users.noreply.github.com>
2023-10-07 10:47:57 -0700

    Assignment 3

---
+++ Assignment 3 - CSTP2104/Assignment 3 - CSTP2104/Country2.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Assignment_3___CSTP2104
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Country2 : Page
+    {
+        public Country2()
+        {
+            this.InitializeComponent();
+        }
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.GoBack(); 
+        }
+    }
+}
