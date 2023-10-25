a1c25251e4acf0ae33cfee8a0213f97ca75905c6
HughEver3tt <59859156+HughEver3tt@users.noreply.github.com>
2023-10-25 12:00:08 -0700

    Assignment 5 Completed

---
+++ Assignment5-2104/Assignment5-2104/Pages/Oppositions.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Assignment5_2104.Pages
+{
+    public class ServiceModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
